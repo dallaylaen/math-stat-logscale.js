f205068079be5dd396d7c17345b5eda53cf36187
Konstantin S. Uvarin <khedin@gmail.com>
2022-03-14 20:40:36 +0200

    [api] Add split-bucket to quantile()

--- test/103-quantile.js
+++ test/103-quantile.js
@@ -5,20 +5,33 @@ const { expect } = require( 'chai' );
 const { Univariate } = require( '../lib/univariate.js' );
 
 describe( 'Univariate.quantile', () => {
+    it ('works for split bucket', done => {
+        const uni = new Univariate({precision:1});
+        uni.add(0);
+
+        const input = [0, 0.25, 0.5, 0.75, 1];
+
+        input.forEach( x => {
+            expect( uni.quantile(x) ).to.be.within(x-0.6, x-0.4);
+        });
+
+        done();
+    });
+
     it ('works for 2 buckets', done => {
         const uni = new Univariate();
         uni.add(1,2);
-        expect(uni.quantile(0.33)).to.equal(uni.round(1));
-        expect(uni.quantile(0.66)).to.equal(uni.round(2));
+        expect(uni.quantile(0.25)).to.be.within(0.99, 1.01);
+        expect(uni.quantile(0.75)).to.be.within(1.99, 2.01);
 
         done();
     });
     it ('works for 3 buckets', done => {
         const uni = new Univariate();
         uni.add(1,2,3);
-        expect(uni.quantile(0.33)).to.equal(uni.round(1));
-        expect(uni.quantile(0.5)).to.equal(uni.round(2));
-        expect(uni.quantile(0.67)).to.equal(uni.round(3));
+        expect(uni.quantile(1/6)).to.be.within(0.99, 1.01);
+        expect(uni.quantile(0.5)).to.be.within(1.99, 2.01);
+        expect(uni.quantile(5/6)).to.be.within(2.99, 3.01);
 
         done();
     });
@@ -29,11 +42,11 @@ describe( 'Univariate.quantile', () => {
         for (let i = 1; i < 20; i++ )
             uni.add(i);
 
-        expect( uni.quantile(0) ).to.equal(uni.round(1));
-        expect( uni.quantile(0.1) ).to.equal(uni.round(2));
-        expect( uni.quantile(0.5) ).to.equal(uni.round(10));
-        expect( uni.quantile(0.9) ).to.equal(uni.round(18));
-        expect( uni.quantile(1) ).to.equal(uni.round(19));
+        expect( uni.quantile(0) ).to.be.within(0.99,1.01);
+        expect( uni.quantile(0.1) ).to.be.within(1.99,2.01);
+        expect( uni.quantile(0.5) ).to.be.within(9.99,10.01);
+        expect( uni.quantile(0.9) ).to.be.within(17.99,18.01);
+        expect( uni.quantile(1) ).to.be.within(18.99,19.02);
 
 
         done();
