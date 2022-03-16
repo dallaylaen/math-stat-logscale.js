ed5fed6fb5dfb7e989b47d145af3020004fb6b69
Konstantin S. Uvarin <khedin@gmail.com>
2022-03-16 15:21:52 +0200

    [repo] Add browserify script

---
+++ aux/browserify.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+set -ex
+
+SRC=./index.js
+DST=docs/js
+MODULE=logscale-stat
+
+webpack-cli "$SRC" --mode development -o "$DST"
+mv "$DST"/main.js "$DST"/"$MODULE".js
+
+webpack-cli "$SRC" --mode production -o "$DST"
+mv "$DST"/main.js "$DST"/"$MODULE".min.js
+
