b29ab37542418afea051ba99a1cd49be23b68965
Konstantin S. Uvarin <khedin@gmail.com>
2022-05-22 23:17:03 +0200

    [test] Add a stupid benchmark example

---
+++ example/speed.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const { Univariate } = require( '../index.js' );
+
+const perf = new Univariate({precision: 0.01});
+
+let measurements = 100;
+let points = 10000;
+
+for (let i = 0; i < measurements; i++) {
+    const t0 = new Date();
+    const stat = new Univariate();
+    for (let j = 0; j < points; j++)
+        stat.add(j);
+    stat.mean();
+    stat.stdev();
+    for (let j = 0; j <= 100; j+= 10)
+        stat.percentile(j);
+    stat.histogram();
+    perf.add( new Date() - t0 );
+}
+
+const hist = perf.histogram({count:16, scale:70})
+    .map( x => perf.shorten(x[1], x[2])+'\t'+'+'.repeat(x[0]) )
+    .join('\n');
+
+console.log(hist);
+
+console.log( 'average: ' + perf.neat.mean() + ' +- ' + perf.neat.stdev() );
+console.log( 'median:  ' + perf.neat.median() );
+console.log( '90%:     ' + perf.neat.percentile(90) );
+
+console.log(JSON.stringify(perf));
