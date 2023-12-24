0625e431e39d263472fc34c829ddafc4414c63dc
Konstantin S. Uvarin <khedin@gmail.com>
2023-12-24 15:25:35 +0100

    [bug] Make clone & toJSON adhere flat: true

--- test/113-uniform-binning.js
+++ test/113-uniform-binning.js
@@ -5,13 +5,21 @@ const { Univariate } = require ( '../index' );
 
 describe( 'Univariate{ uniform: true }', () => {
     it( 'produces distribution without log', done => {
+        const contract = distro => {
+            expect( distro.getBins() ).to.deep.equal(
+                [[10000, 1], [10001, 1], [10002, 1], [10003, 1], [10004, 1], [10005, 1], ] );
+            expect( distro.min() ).to.equal( 9999.5 );
+            expect( distro.max() ).to.equal( 10005.5 );
+        }
+
         const uni = new Univariate({precision: 1, flat: true});
         uni.add( 10000, 10001, 10002, 10003, 10004, 10005);
-        expect( uni.getBins() ).to.deep.equal(
-            [[10000, 1], [10001, 1], [10002, 1], [10003, 1], [10004, 1], [10005, 1], ] );
 
-        expect( uni.min() ).to.equal( 9999.5 );
-        expect( uni.max() ).to.equal( 10005.5 );
+        contract(uni);
+
+        const clone = uni.clone();
+
+        contract(clone);
 
         done();
     });
